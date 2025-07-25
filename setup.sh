b7746ebcfaa90c536cdf8226b60875c987a98c74
Y0U <rubalsharma507@gmail.com>
2025-07-25 12:19:17 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,27 @@
+clear
+
+echo -e "\e[5m\e[1;34m$(figlet 'YOUHON')\e[0m"
+echo Setup Starting........
+
+sudo apt-get install ruby   
+echo ""
+sudo apt install libpcap-dev build-essential
+echo ""
+sudo apt install alsa-utils
+echo ""
+sudo gem install colorize
+echo ""
+sudo gem install openssl
+echo ""
+sudo gem install timeout
+echo ""
+sudo gem install webrick
+echo ""
+sudo gem install packetgen
+echo ""
+sudo gem install optparse
+echo ""
+echo "\n***done***\n"
+clear
+echo ""
+echo Setup Complete GO AHEAD!!!!!!        
